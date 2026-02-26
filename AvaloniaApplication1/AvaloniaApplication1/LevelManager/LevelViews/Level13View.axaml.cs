9d01dc01e62ac50af5d47cd387bfb8e433c7a7e0
dasa <lolosestprsty@protonmail.com>
2026-02-26 03:58:29 +0100

    loadthis

--- AvaloniaApplication1/AvaloniaApplication1/LevelManager/LevelViews/Level13View.axaml.cs
+++ AvaloniaApplication1/AvaloniaApplication1/LevelManager/LevelViews/Level13View.axaml.cs
@@ -10,4 +10,9 @@ public partial class Level13View : UserControl
     {
         InitializeComponent();
     }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
 }
